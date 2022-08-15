c9ba603b0fbf1834b3c32614807e771ded779980
frankieag10 <frankieag10@gmail.com>
2022-08-15 13:06:19 -0700

    completed meme gallery project

---
+++ main.js
@@ -0,0 +1,39 @@
+
+var $gallery = document.querySelector('main');
+
+
+  var imageURLs = [
+  'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+  'https://c.tenor.com/uYP_Nkq8VPsAAAAd/coding-hello-world.gif',
+  'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg'
+
+  ]
+
+
+var $imageUrlInput = document.querySelector('input');
+var $addImageButton = document.querySelector('button');
+
+
+
+$addImageButton.addEventListener('click', function() {
+  if ($imageUrlInput.value !== '') {
+    imageURLs.push($imageUrlInput.value);
+    $imageUrlInput.value = '';
+    updateGallery();
+  }
+});
+
+
+
+function updateGallery() {
+  $gallery.innerHTML = '';
+  for (var i = 0; i < imageURLs.length; i++)
+  {
+    var $imageElement = document.createElement('img');
+    $imageElement.className = 'gallery-image';
+    $imageElement.src = imageURLs[i];
+    $gallery.appendChild($imageElement);
+  }
+}
+
+updateGallery();
